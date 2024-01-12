07ba01341ad5edf8f68b1d80805c9b57419c2d7a
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2024-01-12 19:43:30 +0000

    first commit function ready missing css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [input, setInput] = useState("");
+	const [list, setList] = useState([]);
+
+	const validateInput = (e) => {
+		setInput(e.target.value);
+	};
+
+	const addItem = (e) => {
+		if (e.key === 'Enter') {
+			setList([...list, input]);
+			setInput("");
+		}
+	};
+
+	const deleteItem = (indexItem) => {
+		setList((prevState) =>
+			prevState.filter((elemento, index) => index !== indexItem)
+		);
+	};
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5"> To do list</h1>
+			<input type="text" onChange={validateInput} value={input} onKeyDown={addItem} />
+			<ul>
+				{list.map((elemento, index) => (
+					<li key={index}>
+						{elemento} <i className="fa-solid fa-x" onClick={() => deleteItem(index)}></i>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
 
 export default Home;
+
